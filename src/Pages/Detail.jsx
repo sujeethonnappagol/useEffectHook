d2d62b95e005c49ed082518fa6e5f675250b3399
sujeet <sujeethonnappagol3@gmail.com>
2026-03-02 00:05:26 +0530

    secand commit

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
 function Detail() {
+    useEffect(()=>{
+        console.log("Detail mounted");
+        return()=>{
+            console.log("Detail unmounted");
+        }
+    })   
   return (
     <> 
          <h1 className="text-3xl font-bold underline">This is Detail Page </h1>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,18 @@
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 function Home() {
+    let [data,setData]=useState(0);
     useEffect(()=>{
-        console.log("This is Home Page");
+        console.log("Home mounted");
+        return()=>{
+            console.log("Home unmounted");
+        }
     },[])   
   return (
     <> 
-         <h1 className="text-3xl font-bold underline">This is Home Page </h1>
+        <div className="mx-auto w-[500px] mt-10 ">
+            <h1 className="text-3xl font-bold underline">This is Home Page and my data is {data} </h1>
+            <button className="px-4 py-2 border rounded-lg bg-blue-500 text-center justify-center mt-4" onClick={()=>setData(data+1)}>Update data</button>
+        </div>
     </>
   )
 }
